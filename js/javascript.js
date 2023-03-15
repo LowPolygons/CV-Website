3785e431d22a79064692cf3075bdebad0bb75dde
LowPolygons <127898102+LowPolygons@users.noreply.github.com>
2023-03-15 17:31:15 +0000

    debug

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 function changeColor() {
-  let img = document.getElementById("myDiv").style.backgroundImage;
+  let img = document.getElementById("main-titles").style.backgroundImage;
   document.getElementById("main-photo").style.backgroundImage = img;
   document.getElementById("main-left").style.background = "#999999";
 }
