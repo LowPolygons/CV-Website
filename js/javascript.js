a2c39531f8316be0a3a823176d41c29eadbe4fd3
LowPolygons <127898102+LowPolygons@users.noreply.github.com>
2023-03-15 18:13:31 +0000

    Hexcode shuffle

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,17 @@
-function changeColor() {
-  let img = document.getElementById("main-titles").style.backgroundImage;
-  document.getElementById("main-photo").style.backgroundImage = img;
-  document.getElementById("main-left").style.background = "#999999";
+function random(min, max) {
+	return Math.floor(Math.random()*(max-min) + min);
+}
+
+
+function changeImage() {
+	const vals = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+	var min = 0;
+	var max = 15;
+	var randomImageNum = random(min, max);
+	var printval = ""
+
+	let hexcode = printval.concat("#",vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)]);
+  //var img = document.getElementById("main-titles");
+  //document.getElementById("main-photo").style.backgroundImage = img.Image;
+  document.getElementById("main-left").style.background = hexcode;
 }
