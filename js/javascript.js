f099fcec6496248cac0e05111147754a31c9e205
LowPolygons <127898102+LowPolygons@users.noreply.github.com>
2023-03-15 20:20:54 +0000

    asdasdasdsa

--- js/javascript.js
+++ js/javascript.js
@@ -11,6 +11,7 @@ function determineColourIncrement(colour, increment) {
 }
 
 function RGBtoHex(R,G,B) {
+	const vals = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 	var R2 = R % 16;
 	var R1 = (R-R2) / 16; // 16
 	var G2 = G % 16;
@@ -24,24 +25,22 @@ function RGBtoHex(R,G,B) {
 }
 
 
-const vals = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
-const backgroundColourRGB = [random(0,255),random(0,255),random(0,255)];
-const colourIncrement = 65;
-const textColour = [ 255-backgroundColourRGB[0],255-backgroundColourRGB[1],255-backgroundColourRGB[2] ];
-const boxHighlights = [ determineColourIncrement(backgroundColourRGB[0], colourIncrement),determineColourIncrement(backgroundColourRGB[1], colourIncrement),determineColourIncrement(backgroundColourRGB[2], colourIncrement)];
-const hexBackgroundColour = RGBtoHex(backgroundColourRGB[0],backgroundColourRGB[1],backgroundColourRGB[2]);
-const hexTextColour = RGBtoHex(textColour[0], textColour[1], textColour[2]);
-const hexBoxHighlights = RGBtoHex(boxHighlights[0],boxHighlights[1], boxHighlights[2]);
-
 
 function shuffleStyle() {
-	var min = 0;
-	var max = 15;
-	var randomImageNum = random(min, max);
-	var printval = ""
+	//var min = 0;
+	//var max = 15;
+	//var randomImageNum = random(min, max);
+	//var printval = ""
 
-	let hexcode = printval.concat("#",vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)]);
-	
+	//let hexcode = printval.concat("#",vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)],vals[random(min,max)]);
+	const backgroundColourRGB = [random(0,255),random(0,255),random(0,255)];
+	const colourIncrement = 65;
+	const textColour = [ 255-backgroundColourRGB[0],255-backgroundColourRGB[1],255-backgroundColourRGB[2] ];
+	const boxHighlights = [ determineColourIncrement(backgroundColourRGB[0], colourIncrement),determineColourIncrement(backgroundColourRGB[1], colourIncrement),determineColourIncrement(backgroundColourRGB[2], colourIncrement)];
+	const hexBackgroundColour = RGBtoHex(backgroundColourRGB[0],backgroundColourRGB[1],backgroundColourRGB[2]);
+	const hexTextColour = RGBtoHex(textColour[0], textColour[1], textColour[2]);
+	const hexBoxHighlights = RGBtoHex(boxHighlights[0],boxHighlights[1], boxHighlights[2]);
+		
 	document.getElementById("main-body").style.background = hexBackgroundColour;
 
 	document.getElementById("main-left").style.background = hexBoxHighlights;
@@ -50,5 +49,4 @@ function shuffleStyle() {
 	document.getElementById("main-right").style.background = hexBoxHighlights;
 	document.getElementById("main-right").style.color = hexTextColour;
 }
-
 //<div class = "testbox" onmouseover="changeImage()"></div>
