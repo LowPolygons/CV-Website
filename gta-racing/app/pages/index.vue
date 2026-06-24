a99b26afea2170114f949773f80ee6cb2824db62
Tristan Rimmer <Tristan Rimmer>
2026-06-24 15:48:07 +0100

    Wip: Tailwind styling intro

--- gta-racing/app/pages/index.vue
+++ gta-racing/app/pages/index.vue
@@ -3,11 +3,15 @@ import races from "~/data/races.json"
 </script>
 
 <template>
-    <div>
-        <h1>GTA Racing</h1>
-        <template
+    <div class="bg-gray-300 dark:bg-gray-700">
+        <h1 class="text-slate-900 dark:text-slate-200 text-center p-1 text-3xl font-bold bg-gray-400 dark:bg-gray-900">
+            GTA Racing
+        </h1>
+        <div
             v-for="(race, index) in races"
             :key="race.name"
+            class="w-9/10 mx-auto p-1"
+            
         >
             <Race
                 :name="race.name"
@@ -15,6 +19,12 @@ import races from "~/data/races.json"
                 @click="navigateTo(`/${index}`)"
             />
             <hr v-if="index !== races.length - 1">
-        </template>
+        </div>
     </div>
 </template>
+
+<style scoped lang="scss">
+.test {
+    @apply text-slate-900 dark:text-slate-200 text-center p-1 text-3xl font-bold bg-gray-400 dark:bg-gray-900;
+}
+</style>
